b63968a49ee3ef46d90bf87f6ea9f014f8e87bbf
vlad <vm@delai-delo.by>
2024-07-01 11:57:54 +0300

    Fix properties and connections

--- commerce/credit/src/main/java/com/banksystem/overdue/service/OverdueChecker.java
+++ commerce/credit/src/main/java/com/banksystem/overdue/service/OverdueChecker.java
@@ -2,7 +2,7 @@ package com.banksystem.overdue.service;
 
 import com.banksystem.credit.service.CreditService;
 import com.banksystem.overdue.dto.OverdueDto;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,12 +10,18 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class OverdueChecker {
     private final CreditService creditService;
     private final String kafkaTopic;
     private final KafkaTemplate<String, OverdueDto> kafkaTemplate;
 
+    public OverdueChecker(final CreditService creditService,
+                          final @Value("${topics.overdue.name}") String kafkaTopic,
+                          final KafkaTemplate<String, OverdueDto> kafkaTemplate) {
+        this.creditService = creditService;
+        this.kafkaTopic = kafkaTopic;
+        this.kafkaTemplate = kafkaTemplate;
+    }
     @Scheduled(fixedRate = 10000)
     public void checkOverdue(){
         List<OverdueDto> overdueDtos = creditService.checkOverdue();
