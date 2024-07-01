4db97fd0a7ea541110503d44e23a3c943bd5c739
vlad <vm@delai-delo.by>
2024-07-01 12:13:46 +0300

    Fix kafka producer config

--- commerce/credit/src/main/java/com/banksystem/tech/kafka/KafkaConfig.java
+++ commerce/credit/src/main/java/com/banksystem/tech/kafka/KafkaConfig.java
@@ -2,8 +2,8 @@ package com.banksystem.tech.kafka;
 
 import com.banksystem.overdue.dto.OverdueDto;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -11,26 +11,43 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
 
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+    @Value("${spring.kafka.producer.acks}")
+    private String acks;
 
+    Map<String, Object> producerConfigs() {
+        HashMap<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        config.put(ProducerConfig.ACKS_CONFIG, acks);
 
-    @Bean
-    public ProducerFactory<String, OverdueDto> producerFactory(KafkaProperties kafkaProperties){
-        final Map<String, Object> properties = kafkaProperties.buildProducerProperties();
-        return new DefaultKafkaProducerFactory<>(properties);
+        return config;
     }
 
     @Bean
-    public KafkaTemplate<String, OverdueDto> kafkaTemplate(ProducerFactory<String, OverdueDto> producerFactory){
-        return new KafkaTemplate<>(producerFactory);
+    ProducerFactory<String, OverdueDto> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
     }
 
     @Bean
-    public NewTopic createTopic(@Value("${topics.overdue.name}") String kafkaTopic){
+    public KafkaTemplate<String, OverdueDto> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    NewTopic createTopic(@Value("${topics.overdue.name}") String kafkaTopic){
         return TopicBuilder
                 .name(kafkaTopic)
                 .partitions(3)
